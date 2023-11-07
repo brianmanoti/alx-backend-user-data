7c14b18c5ba10238a4969105b16369ce25926dc3
brianmanoti <brianmanoti254@gmail.com>
2023-11-07 09:42:32 +0000

    Trial

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,12 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized"""
+    abort(401)
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
     Return:
@@ -13,6 +18,10 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden"""
+    abort(403)
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -24,22 +33,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - Aborts
-    """
-    abort(401)
-
-
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """
-    GET /api/v1/unauthorized
-    Return:
-      - Aborts
-    """
-    abort(403)
