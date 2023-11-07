315dc2cc155e915f12b8ea894e3364985a5a71e1
brianmanoti <brianmanoti254@gmail.com>
2023-11-07 09:40:20 +0000

    Trial

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
@@ -35,7 +35,7 @@ def unauthorized() -> str:
     abort(401)
 
 
-@app_views.route('/forbidden', strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """
     GET /api/v1/unauthorized
