6b9742e82fe4dfcbb0fc46721e449e4c632bfcb2
brianmanoti <brianmanoti254@gmail.com>
2023-11-07 09:54:07 +0000

    Authentication Class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+from flask import request
+
+
+class Auth:
+    """ classs to handle Authentication """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require authentication
+        return:
+            -return Flase
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header to return None """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Checks The current user and returns the object """
+        return None
