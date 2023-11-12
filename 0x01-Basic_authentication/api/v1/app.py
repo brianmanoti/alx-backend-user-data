94adcbbab1395b584939eb33a132a3cc7fe6b4a6
brianmanoti <brianmanoti254@gmail.com>
2023-11-12 21:40:24 +0000

    Fixing bugs

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,22 +4,44 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.auth import Auth
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
 
-if getenv("AUTH_TYPE") == "auth":
-    auth = Auth()
-elif getenv("AUTH_TYPE") == "basic_auth":
+
+auth = None
+auth_type = os.environ.get('AUTH_TYPE')
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """Handles authorization
+    """
+    if auth is None:
+        return
+
+    allowed_paths = [
+            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth.require_auth(request.path, allowed_paths):
+
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
+    return
 
 
 @app.errorhandler(404)
@@ -41,21 +63,6 @@ def unauthorized(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.before_request
-def before_request():
-    """
-    handler before_request
-    """
-    authorized_list = ['/api/v1/status',
-                       '/api/v1/unauthorized/', '/api/v1/forbidden']
-
-    if auth and auth.require_auth(request.path, authorized_list):
-        if not auth.authorization_header(request):
-            abort(401)
-        if not auth.current_user(request):
-            abort(403)
 
 
 if __name__ == "__main__":
